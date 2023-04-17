b762160623aa1abbead7e1ed63b10c545b18b40d
Sukhvinder Singh <sukhvindersingh@Sukhvinders-MacBook-Air.local>
2023-04-17 12:49:55 -0400

    user creation works

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2"
 
 @Injectable()
 export class AuthService{
     constructor(private prismaService: PrismaService){}
     
-    signup(){
-        return 'This is Signup Route.'
+    async signup(dto: AuthDto){
+        // generate the new password from hash
+        const hash = await argon.hash(dto.password.toString());
+
+        const userData = {
+            email: dto.email.toString(),
+            hash,
+        }
+        // save the new user
+        const user = await this.prismaService.user.create({
+            data: userData
+        })
+
+        // return the new user creation
+        return user
     }
     login(){
         return 'This is Login Route.'
